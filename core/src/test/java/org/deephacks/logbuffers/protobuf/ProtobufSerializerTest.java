461de8734cf345eaac877eeb4f9c671111eb0dc8
Kristoffer Sjogren <stoffe@gmail.com>
2013-12-28 15:56:00 +0100

    added tail test for protobufs

--- core/src/test/java/org/deephacks/logbuffers/protobuf/ProtobufSerializerTest.java
+++ core/src/test/java/org/deephacks/logbuffers/protobuf/ProtobufSerializerTest.java
@@ -4,6 +4,8 @@ package org.deephacks.logbuffers.protobuf;
 import org.deephacks.logbuffers.LogBuffer;
 import org.deephacks.logbuffers.LogBuffer.Builder;
 import org.deephacks.logbuffers.ObjectLogBuffer;
+import org.deephacks.logbuffers.ObjectLogBufferTail;
+import org.deephacks.logbuffers.Tail;
 import org.deephacks.logbuffers.protobuf.ProtoLog.PageView;
 import org.deephacks.logbuffers.protobuf.ProtoLog.Visit;
 import org.deephacks.logbuffers.util.TestUtil;
@@ -12,6 +14,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.core.Is.is;
@@ -20,12 +23,28 @@ import static org.junit.Assert.*;
 public class ProtobufSerializerTest {
 
     ObjectLogBuffer objectLogBuffer;
+    PageViewTail pageViewTail;
+    VisitTail visitTail;
+
+    ObjectLogBufferTail<PageView> pageViewBufferTail;
+    ObjectLogBufferTail<Visit> visitBufferTail;
+
     LogBuffer logBuffer;
+
+    PageView p1 = PageView.newBuilder().setMsg("hello1").setCode(1).build();
+    PageView p2 = PageView.newBuilder().setMsg("hello2").setCode(2).build();
+    Visit v1 = Visit.newBuilder().setMsg("vist1").setCode(1).build();
+    Visit v2 = Visit.newBuilder().setMsg("vist2").setCode(2).build();
+
 
     @Before
     public void before() throws IOException {
         logBuffer = new Builder().basePath(TestUtil.tmpDir()).build();
         objectLogBuffer = new ObjectLogBuffer(logBuffer, new ProtobufSerializer());
+        pageViewTail = new PageViewTail();
+        visitTail = new VisitTail();
+        pageViewBufferTail = new ObjectLogBufferTail<>(objectLogBuffer, pageViewTail);
+        visitBufferTail = new ObjectLogBufferTail<>(objectLogBuffer, visitTail);
     }
 
     @After
@@ -36,8 +55,6 @@ public class ProtobufSerializerTest {
     @Test
     public void test_write_read_different_types() throws IOException {
         // one type log
-        PageView p1 = PageView.newBuilder().setMsg("hello1").setCode(1).build();
-        PageView p2 = PageView.newBuilder().setMsg("hello2").setCode(2).build();
         objectLogBuffer.write(p1);
         objectLogBuffer.write(p2);
         List<PageView> pageViews = objectLogBuffer.select(PageView.class, 0);
@@ -48,8 +65,6 @@ public class ProtobufSerializerTest {
         assertThat(pageViews.get(1).getCode(), is(p2.getCode()));
 
         // another type log
-        Visit v1 = Visit.newBuilder().setMsg("vist1").setCode(1).build();
-        Visit v2 = Visit.newBuilder().setMsg("vist2").setCode(2).build();
         objectLogBuffer.write(v1);
         objectLogBuffer.write(v2);
         List<Visit> visits = objectLogBuffer.select(Visit.class, 0);
@@ -59,4 +74,45 @@ public class ProtobufSerializerTest {
         assertThat(visits.get(1).getMsg(), is(v2.getMsg()));
         assertThat(visits.get(1).getCode(), is(v2.getCode()));
     }
+
+    @Test
+    public void test_write_tail_different_types() throws IOException {
+
+        objectLogBuffer.write(p1);
+        objectLogBuffer.write(v1);
+        objectLogBuffer.write(p2);
+        objectLogBuffer.write(v2);
+
+        visitBufferTail.forward();
+        assertThat(visitTail.logs.size(), is(2));
+        assertThat(pageViewTail.logs.size(), is(0));
+        assertThat(visitTail.logs.get(0).getMsg(), is(v1.getMsg()));
+        assertThat(visitTail.logs.get(1).getMsg(), is(v2.getMsg()));
+
+        pageViewBufferTail.forward();
+        assertThat(visitTail.logs.size(), is(2));
+        assertThat(pageViewTail.logs.size(), is(2));
+        assertThat(pageViewTail.logs.get(0).getMsg(), is(p1.getMsg()));
+        assertThat(pageViewTail.logs.get(1).getMsg(), is(p2.getMsg()));
+
+    }
+
+    public static class PageViewTail implements Tail<PageView> {
+        List<PageView> logs = new ArrayList<>();
+
+        @Override
+        public void process(List<PageView> logs) {
+            this.logs.addAll(logs);
+        }
+    }
+
+    public static class VisitTail implements Tail<Visit> {
+        List<Visit> logs = new ArrayList<>();
+
+        @Override
+        public void process(List<Visit> logs) {
+            this.logs.addAll(logs);
+        }
+    }
+
 }
