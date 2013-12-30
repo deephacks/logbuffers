e304207998173c0e386a4aa0aaa7bc6f9b392d3c
Kristoffer Sjogren <stoffe@gmail.com>
2013-12-30 01:49:29 +0100

    align logbuffer test

--- core/src/test/java/org/deephacks/logbuffers/LogBufferTest.java
+++ core/src/test/java/org/deephacks/logbuffers/LogBufferTest.java
@@ -2,18 +2,21 @@ package org.deephacks.logbuffers;
 
 
 import org.deephacks.logbuffers.LogBuffer.Builder;
+import org.deephacks.logbuffers.LogUtil.TailLog;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class LogBufferTest {
   LogBuffer logBuffer;
+  TailLog tail;
   Log log1 = LogUtil.randomLog(1);
   Log log2 = LogUtil.randomLog(2);
   Log log3 = LogUtil.randomLog(3);
@@ -23,10 +26,12 @@ public class LogBufferTest {
   @Before
   public void before() throws IOException {
     logBuffer = new Builder().basePath(LogUtil.tmpDir()).build();
+    tail = new TailLog();
   }
 
   @After
   public void after() throws IOException {
+    logBuffer.cancel(TailLog.class);
     logBuffer.close();
   }
 
@@ -90,5 +95,56 @@ public class LogBufferTest {
     assertThat(select.get(0), is(log3));
     assertThat(select.get(1), is(log4));
     assertThat(select.get(2), is(log5));
+  }
+
+
+  @Test
+  public void test_manual_forward() throws IOException {
+    // one log
+    log1 = logBuffer.write(log1);
+    logBuffer.forward(tail);
+    assertThat(tail.logs.size(), is(1));
+    assertThat(tail.logs.get(0), is(log1));
+
+    // write another
+    log2 = logBuffer.write(log2);
+    logBuffer.forward(tail);
+    assertThat(tail.logs.size(), is(2));
+    assertThat(tail.logs.get(1), is(log2));
+
+    // write multiple
+    log1 = logBuffer.write(log1);
+    log2 = logBuffer.write(log2);
+    logBuffer.forward(tail);
+    assertThat(tail.logs.size(), is(4));
+    assertThat(tail.logs.get(2), is(log1));
+    assertThat(tail.logs.get(3), is(log2));
+  }
+
+
+  @Test
+  public void test_scheduled_forward() throws Exception {
+    logBuffer.forwardWithFixedDelay(tail, 500, TimeUnit.MILLISECONDS);
+    // one log
+    log1 = logBuffer.write(log1);
+    Thread.sleep(600);
+    assertThat(tail.logs.size(), is(1));
+    assertThat(tail.logs.get(0), is(log1));
+
+    // write another
+    log2 = logBuffer.write(log2);
+    Thread.sleep(600);
+
+    assertThat(tail.logs.size(), is(2));
+    assertThat(tail.logs.get(1), is(log2));
+
+    // write multiple
+    log1 = logBuffer.write(log1);
+    log2 = logBuffer.write(log2);
+    Thread.sleep(600);
+
+    assertThat(tail.logs.size(), is(4));
+    assertThat(tail.logs.get(2), is(log1));
+    assertThat(tail.logs.get(3), is(log2));
   }
 }
