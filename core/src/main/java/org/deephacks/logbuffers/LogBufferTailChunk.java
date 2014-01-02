929ec0bc6947271b09ed94339410087632b64ad9
Kristoffer Sjogren <stoffe@gmail.com>
2014-01-02 22:32:45 +0100

    always scanning tails forward is simpler

--- core/src/main/java/org/deephacks/logbuffers/LogBufferTailChunk.java
+++ core/src/main/java/org/deephacks/logbuffers/LogBufferTailChunk.java
@@ -3,25 +3,16 @@ package org.deephacks.logbuffers;
 import com.google.common.base.Optional;
 
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * Specialized tail that provide logs iteratively in chunks according to a certain period of time.
  */
 class LogBufferTailChunk<T> extends LogBufferTail<T> {
   private long chunkMs;
-  public static SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss:SSS");
-  private final Logger logger;
-  private Optional<Log> moreWork;
 
   LogBufferTailChunk(LogBuffer logBuffer, Tail<T> tail, long chunkMs) throws IOException {
     super(logBuffer, tail);
     this.chunkMs = chunkMs;
-    this.logger = Logger.getLogger(getClass().getName());
-    this.moreWork = Optional.absent();
   }
 
   @Override
@@ -30,7 +21,6 @@ class LogBufferTailChunk<T> extends LogBufferTail<T> {
     // writer index is so tail can speed up if backlog is too big.
     Optional<Log> latestWrite = logBuffer.getLatestWrite();
     if (!latestWrite.isPresent()) {
-      moreWork = Optional.absent();
       return new ForwardResult();
     }
 
@@ -38,7 +28,6 @@ class LogBufferTailChunk<T> extends LogBufferTail<T> {
     long currentReadIndex = getReadIndex();
     Optional<Log> currentLog = logBuffer.get(currentReadIndex);
     if (!currentLog.isPresent()) {
-      moreWork = Optional.absent();
       return new ForwardResult();
     }
 
@@ -48,19 +37,14 @@ class LogBufferTailChunk<T> extends LogBufferTail<T> {
     // do not process ahead of time, meaning tail will not try process
     // logs until the chunkMs have passed since the present.
     if (fixedTo > System.currentTimeMillis()) {
-      moreWork = Optional.absent();
       return new ForwardResult();
     }
-    Logs<T> logs;
-    if (moreWork.isPresent()) {
-      logs = logBuffer.selectForward(type, moreWork.get().getIndex(), fixedFrom, fixedTo);
-    } else {
-      logs = logBuffer.selectBackward(type, fixedFrom, fixedTo);
-    }
-    logger.log(Level.FINE, format.format(new Date(fixedFrom)) + " " + format.format(new Date(fixedTo)));
+
+    // start directly at index where processing ended previous round go forward in time
+    Logs<T> logs = logBuffer.selectForward(type, currentLog.get().getIndex(), fixedFrom, fixedTo);
+
     // don't call tail if there are no logs
     if (logs.isEmpty()) {
-      moreWork = Optional.absent();
       return new ForwardResult();
     }
     // prepare the next read index BEFORE we hand over logs to tail
@@ -68,12 +52,10 @@ class LogBufferTailChunk<T> extends LogBufferTail<T> {
     long lastReadIndex = lastRead.getIndex();
     // prepare result
     ForwardResult result = new ForwardResult();
-    moreWork = Optional.absent();
     if (lastRead.getTimestamp() < latestWrite.get().getTimestamp()) {
       // alter the result to indicate that there are already more logs
       // to process after this round have been executed. Hence we can
       // act quickly and process these as fast as possible, if needed.
-      this.moreWork = Optional.fromNullable(lastRead);
       result = new ForwardResult(false);
     }
     try {
@@ -84,7 +66,6 @@ class LogBufferTailChunk<T> extends LogBufferTail<T> {
       // only write/persist last read index if tail was successful
       writeReadIndex(lastReadIndex + 1);
     } catch (Exception e) {
-      this.moreWork = Optional.absent();
       result = new ForwardResult();
       System.err.println(e.getMessage());
     }
