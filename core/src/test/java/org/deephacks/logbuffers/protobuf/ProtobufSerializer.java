814ad66f82acd70332146f71c5c99c276fa205eb
Kristoffer Sjogren <stoffe@gmail.com>
2014-01-03 15:04:10 +0100

    optimize protobuf serializer

--- core/src/test/java/org/deephacks/logbuffers/protobuf/ProtobufSerializer.java
+++ core/src/test/java/org/deephacks/logbuffers/protobuf/ProtobufSerializer.java
@@ -9,14 +9,27 @@ import org.deephacks.logbuffers.protobuf.ProtoLog.PageView;
 import org.deephacks.logbuffers.protobuf.ProtoLog.Visit;
 
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ProtobufSerializer implements ObjectLogSerializer {
 
   private BiMap<Long, Class<?>> mapping = HashBiMap.create();
+  private Map<Long, Method> methodCache = new HashMap<>();
 
   public ProtobufSerializer() {
     mapping.put(120L, PageView.class);
+    methodCache.put(120L, getParseMethod(PageView.class));
     mapping.put(121L, Visit.class);
+    methodCache.put(121L, getParseMethod(Visit.class));
+  }
+
+  private Method getParseMethod(Class<?> cls) {
+    try {
+      return cls.getMethod("parseFrom", byte[].class);
+    } catch (NoSuchMethodException e) {
+      throw new IllegalArgumentException(e);
+    }
   }
 
   @Override
@@ -33,8 +46,7 @@ public class ProtobufSerializer implements ObjectLogSerializer {
   @Override
   public Object deserialize(byte[] log, long type) {
     try {
-      Method parseFrom = mapping.get(type).getMethod("parseFrom", byte[].class);
-      return parseFrom.invoke(null, log);
+      return methodCache.get(type).invoke(null, log);
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
